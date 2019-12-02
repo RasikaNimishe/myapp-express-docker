379ecd322d3d08d9c1016978f8c8103735b39125
rasikanimishe <hrutikanimishe@gmail.com>
2019-12-02 15:54:01 +0530

    initial version

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql=require('mysql')
 function connect()
 {
     const connection=mysql.createConnection({
-        host:'172.17.0.1',
+        host:'172.18.5.76',
         user:'root',
         password:'manager',
         database:'dac_db',
